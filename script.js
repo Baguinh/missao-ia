b026d52d231e31ee7cbec962c0d6320a7e14c428
Gabriela Catarino <catarino.correia@escola.pr.gov.br>
2026-08-21 08:52:53 -0300

    llll

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = ["Pergunta 1", "Pergunta 2"];
 const perguntas = [
   {
     enunciado: "Pergunta 1",
